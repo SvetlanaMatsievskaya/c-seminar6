7bd8fa6adffec8e5e3a94c7a0ef2e3407db316e3
svetlana matsyevskaya <svetlanamats@mail.ru>
2022-08-05 10:40:47 +0300

    Task41 added

---
+++ Task41/Program.cs
@@ -0,0 +1,34 @@
+﻿int m = 0;
+Console.Write("Введите количество чисел: ");
+int.TryParse(Console.ReadLine(), out m);
+
+int[] array = new int[m];
+for (int i = 0; i < m; i++)
+{
+  
+  Console.Write($"Введите {i+1} число : ");
+  array[i] = int.Parse(Console.ReadLine());
+  
+}
+
+void PrintArray(int[] arr)
+{
+  for (int i = 0; i < m; i++)
+  {
+      
+    Console.Write($"{arr[i]} ");
+       
+  }
+}
+PrintArray(array);
+
+int CountPositiveNumbers (int [] array)
+{
+  int count = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i]>0) count+=1;
+  }
+  return count;
+}
+Console.WriteLine($"-> {CountPositiveNumbers(array)}");
